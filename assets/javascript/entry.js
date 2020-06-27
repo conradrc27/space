5bcbbbe07b2658d537d1850dabf804347dee8b01
conradoRC <conrado27cym@gmail.com>
2020-06-27 14:04:00 -0500

    Menu ok

--- assets/javascript/entry.js
+++ assets/javascript/entry.js
@@ -2,4 +2,11 @@ import '../css/app.scss';
 
 import './slider/sliderDOM';
 
-import './slider/maps';
+document.querySelector(".menu").addEventListener("click",function(){
+    document.querySelector(".menu-screen").classList.add("active");
+})
+
+document.querySelector(".close").
+addEventListener("click",function(){
+    document.querySelector(".menu-screen").classList.remove("active");
+})
