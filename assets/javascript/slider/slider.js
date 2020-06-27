ada73a140249a29ea8f809d00a5d5fe43993e393
conradoRC <conrado27cym@gmail.com>
2020-06-27 10:16:35 -0500

    Arrow-Navigation

--- assets/javascript/slider/slider.js
+++ assets/javascript/slider/slider.js
@@ -7,24 +7,43 @@ export default class Slider{
         this.size= elements.length; 
         
         this.speed = speed;
-        this.prev = this.prev.bind(this);
+        
+        this.innerNext = this.innerNext.bind(this);
         this.stop = this.stop.bind(this);
-    }
 
-    next(){
+        this.next = this.next.bind(this);
+        this.prev = this.prev.bind(this);
+    }   
+
+    innerNext(){
         this.index++;
         if(this.index>= this.size) this.index= 0;
         this.animationFunc(this.elements[this.index]);
     }
-    prev(){
+    innerPrev(){
         this.index--;
         if(this.index<0) this.index = this.size - 1;
         this.animationFunc(this.elements[this.index]);
     }
 
+    next(){
+        this.innerNext();
+        if(this.interval){
+            this.stop();
+            this.play();
+        }
+    }
+
+    prev(){
+        this.innerPrev();
+        if(this.interval){
+            this.stop();
+            this.play();
+        }
+    }
+
     play(){
-        console.log(this);
-        this.interval = setInterval(this.prev,this.speed)
+        this.interval = setInterval(this.innerNext,this.speed)
     }
 
     stop(){
