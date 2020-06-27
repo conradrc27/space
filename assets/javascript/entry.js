8db049dae723dc0a03faf8f926ad16a9d462d211
conradoRC <conrado27cym@gmail.com>
2020-06-27 14:25:58 -0500

    Menu with scroll effect

--- assets/javascript/entry.js
+++ assets/javascript/entry.js
@@ -1,12 +1,3 @@
 import '../css/app.scss';
-
 import './slider/sliderDOM';
-
-document.querySelector(".menu").addEventListener("click",function(){
-    document.querySelector(".menu-screen").classList.add("active");
-})
-
-document.querySelector(".close").
-addEventListener("click",function(){
-    document.querySelector(".menu-screen").classList.remove("active");
-})
+import './menu';
